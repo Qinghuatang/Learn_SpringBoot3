452055dbd7a2683cb4f36dd50ad561c4055134c9
Qinghuatang <1822064204@qq.com>
2024-11-27 20:21:27 +0800

    使用IDEA工具的脚手架插件

---
+++ springboot3-03-use-idea-plugin/src/test/java/com/qinghua/springboot/Springboot303UseIdeaPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinghua.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot303UseIdeaPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
