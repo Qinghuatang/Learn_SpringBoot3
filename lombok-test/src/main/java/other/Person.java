577b1287b261c05954374e512731d87a1b5067cd
Qinghuatang <1822064204@qq.com>
2024-11-29 10:37:31 +0800

    lombok中的@Singular注解

--- lombok-test/src/main/java/other/Person.java
+++ lombok-test/src/main/java/other/Person.java
@@ -2,10 +2,16 @@ package other;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
+
+import java.util.List;
 
 @Builder    // 帮助这个类生成符合构造者模式的代码
 @Data
 public class Person {
     private String name;
     private int age;
+
+    @Singular("addPhone")
+    private List<String> phones;
 }
--- lombok-test/src/main/java/other/Test.java
+++ lombok-test/src/main/java/other/Test.java
@@ -1,11 +1,33 @@
 package other;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+        /*Person person = Person.builder()
+                .name("李四")
+                .age(20)
+                .build();*/
+
+        /*List<String> phones = new ArrayList<>();
+        phones.add("123456789");
+        phones.add("987654321");
+
+
         Person person = Person.builder()
                 .name("李四")
                 .age(20)
+                .phones(phones)
+                .build();*/
+
+        Person person = Person.builder()
+                .name("李四")
+                .age(20)
+                .addPhone("123456789")
+                .addPhone("987654321")
                 .build();
+
         System.out.println(person);
     }
 }
